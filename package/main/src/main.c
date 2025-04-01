94215258dcbeaa15b02b5afdb4b0c938dffb9a93
gasc <gaudenz.schweizer@icloud.com>
2025-04-02 01:07:57 +0200

    working on linker not finding .isr_vector

--- package/main/src/main.c
+++ package/main/src/main.c
@@ -1,5 +1,18 @@
-
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/wdt.h>
 
 void main(void){
 
+    DDRB |= (1 << 5);
+    PORTB |= (1<<5);
+    _delay_ms(1900);
+
+    while(1){
+        PINB |= (1 << 5);
+
+        _delay_ms(200);
+    }
+
 }
+
