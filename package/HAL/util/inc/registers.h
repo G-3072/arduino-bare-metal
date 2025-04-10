2d50ffb4157835579905c1211bc12a6ae16a36e5
Gaudenz Schweizer <gaudenz.schweizer@cedes.com>
2025-04-10 09:00:26 +0200

    finished register defines

--- package/HAL/util/inc/registers.h
+++ package/HAL/util/inc/registers.h
@@ -16,6 +16,13 @@
 
 #define EIFR    (*(uint8_t *)0x3D)
 #define EIMSK   (*(uint8_t *)0x3C)
+#define EEICRA  (*(uint8_t *)0x69)
+
+#define PCICR   (*(uint8_t *)0x68)
+
+#define PCMSK0  (*(uint8_t *)0x6B)
+#define PCMSK1  (*(uint8_t *)0x6C)
+#define PCMSK2  (*(uint8_t *)0x6D)
 
 //timer registers
 #define TIFR1   (*(uint8_t *)0x36)
@@ -29,6 +36,39 @@
 #define TCMT0   (*(uint8_t *)0x46)
 #define OCR0A   (*(uint8_t *)0x47)
 #define OCR0B   (*(uint8_t *)0x48)
+
+#define TIMSK0   (*(uint8_t *)0x6E)
+#define TIMSK1   (*(uint8_t *)0x6F)
+#define TIMSK2   (*(uint8_t *)0x70)
+
+#define TCCR1A   (*(uint8_t *)0x80)
+#define TCCR1B   (*(uint8_t *)0x81)
+#define TCCR1C   (*(uint8_t *)0x82)
+
+#define TCNT1L   (*(uint8_t *)0x84)
+#define TCNT1H   (*(uint8_t *)0x85)
+#define TCNT1    (*(uint16_t *)0x84)
+
+#define ICR1L    (*(uint8_t *)0x86)
+#define ICR1H    (*(uint8_t *)0x87)
+#define ICR1     (*(uint16_t *)0x86)
+
+#define OCR1AL   (*(uint8_t *)0x88)
+#define OCR1AH   (*(uint8_t *)0x89)
+#define OCR1A    (*(uint16_t *)0x88)
+
+#define OCR1BL   (*(uint8_t *)0x8A)
+#define OCR1BH   (*(uint8_t *)0x8B)
+#define OCR1B    (*(uint16_t *)0x8A)
+
+#define TCCR2A   (*(uint8_t *)0xB0)
+#define TCCR2B   (*(uint8_t *)0xB1)
+#define TCNT2    (*(uint8_t *)0xB2)
+#define OCR2A    (*(uint8_t *)0xB3)
+#define OCR2B    (*(uint8_t *)0xB4)
+
+#define ASSR     (*(uint8_t *)0xB5)
+
 
 //EEPROM registers
 #define EECR   (*(uint8_t *)0x3F)
@@ -57,4 +97,37 @@
 //clock registers
 #define CLKPR   (*(uint8_t *)0x61)
 #define OSCAL   (*(uint8_t *)0x66)
+
+// ADC registers
+#define ADCL   (*(uint8_t *)0x78)
+#define ADCH   (*(uint8_t *)0x79)
+#define ADC    (*(uint16_t *)0x78)
+
+#define ADCSRA (*(uint8_t *)0x7A)
+#define ADCSRB (*(uint8_t *)0x7B)
+#define ADMUX  (*(uint8_t *)0x7C)
+
+#define DIDR0   (*(uint8_t *)0x7E)
+#define DIDR1   (*(uint8_t *)0x7F)
+
+//TWI registers
+#define TWBR    (*(uint8_t *)0xB8)
+#define TWSR    (*(uint8_t *)0xB9)
+#define TWAR    (*(uint8_t *)0xBA)
+#define TWDR    (*(uint8_t *)0xBB)
+#define TWCR    (*(uint8_t *)0xBC)
+#define TWAMR   (*(uint8_t *)0xBD)
+
+//USART registers
+#define UCSR0A    (*(uint8_t *)0xC0)
+#define UCSR0A    (*(uint8_t *)0xC1)
+#define UCSR0A    (*(uint8_t *)0xC2)
+
+#define UBRR0L    (*(uint8_t *)0xC4)
+#define UBRR0H    (*(uint8_t *)0xC5)
+#define UBRR0     (*(uint16_t *)0xC4)
+
+#define UDR0    (*(uint8_t *)0xC6)
+
+
 #endif
